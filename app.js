4f6fe2ef903ac3f57e429a62772ee8ebcd21e136
szymon <szymon.mich@gmail.com>
2020-01-25 22:10:37 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const { App } = require('@slack/bolt')
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message('hello', ({ message, say }) => {
+    // say(`hey <@${message.user}>`)
+    say({
+        blocks: [
+            {
+                "type": "section",
+                "text": {
+                    type: "mrkdwn",
+                    text: `hey <@${message.user}>`
+                },
+                accessory: {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "Click me"
+                    },
+                    action_id: "button_click"
+                } 
+            }
+        ]
+    })
+});
+
+app.action('button_click', ({ body, ack, say }) => {
+    ack()
+    say(`<@${body.user.id}> clicked the button`)
+});
+
+
+(async () => {
+    await app.start(process.env.PORT || 3000)
+    console.log('app is running')
+})()
+
+module.exports = app
---
+++ tests/dummy.test.js
@@ -0,0 +1,5 @@
+describe('First test', () => {
+    it('sends a hello msg', () => {
+        expect(true).toBe(true)
+    })
+})
